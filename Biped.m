109ccb00171686d37f2724ed262413bb872d6561
jzheng84 <jzheng84@gatech.edu>
2017-09-30 17:54:16 -0400

    Fixed frames to match with website's convention, and plotted limbs between joint frames.

--- Biped.m
+++ Biped.m
@@ -47,7 +47,7 @@ classdef Biped < handle
     function obj = Biped( )
         % TO DO: Assign default values for any class properties that do
         %        not otherwise have associated 'setter' class methods
-        obj.torsoSE2 = SE2([0, 0], -pi/2);
+        obj.torsoSE2 = SE2([0, 0], 0);
         obj.gRO = obj.torsoSE2.getM();
         %Initialize Biped with 0 angles on all joints
         obj.alphaL = [0 0 0];
@@ -81,15 +81,21 @@ classdef Biped < handle
       % TO DO: Compute forward kinematics (FK) of the left and right foot
       %        frames, relative to the torso frame, based on the current
       %        joint configuration (angles) of the biped
-      g_torso_p1L = SE2([obj.linkLeft(1), 0], obj.alphaL(1));
-      g_p1L_p2L = SE2([obj.linkLeft(2), 0], obj.alphaL(2));
-      g_p2L_Lf = SE2([obj.linkLeft(3), 0], obj.alphaL(3));
+      g_torso_p1L = SE2([0, -obj.linkLeft(1)], obj.alphaL(1));
+      g_p1L_p2L = SE2([0, -obj.linkLeft(2)], obj.alphaL(2));
+      g_p2L_Lf = SE2([0, -obj.linkLeft(3)], obj.alphaL(3));
       g_t_lf = g_torso_p1L * g_p1L_p2L * g_p2L_Lf;
       
-      g_torso_p1R = SE2([obj.linkRight(1), 0], obj.alphaR(1));
-      g_p1R_p2R = SE2([obj.linkRight(2), 0], obj.alphaR(2));
-      g_p2R_Rf = SE2([obj.linkRight(3), 0], obj.alphaR(3));
+      g_torso_p1R = SE2([0, -obj.linkRight(1)], obj.alphaR(1));
+      g_p1R_p2R = SE2([0, -obj.linkRight(2)], obj.alphaR(2));
+      g_p2R_Rf = SE2([0, -obj.linkRight(3)], obj.alphaR(3));
       g_t_rf = g_torso_p1R * g_p1R_p2R * g_p2R_Rf;
+    end
+    
+    function plot_limb(object, tf1, tf2)
+        p1 = tf1 * [0;0;1];
+        p2 = tf1 * tf2(:,3);
+        plot([p1(1) p2(1)], [p1(2) p2(2)], 'r-')
     end
     
     % Plot the torso frame, both foot frames and all links, consistent with
@@ -99,29 +105,57 @@ classdef Biped < handle
         %        all links of the biped
         figure
         hold on
+        %Plot torso (same for left and right)
         obj.torsoSE2.plot('torso')
-        g_torso_p1L = obj.torsoSE2 * SE2([obj.linkLeft(1), 0], obj.alphaL(1));
+        %Plot first left joint
+        g_torso_p1L = obj.torsoSE2 * SE2([0, -obj.linkLeft(1)], obj.alphaL(1));
         g_torso_p1L.plot('p1L')
         hold on
-        g_p1L_p2L = SE2([obj.linkLeft(2), 0], obj.alphaL(2));
+            %Plot line between torso and first left joint
+            obj.plot_limb(obj.torsoSE2.getM(), g_torso_p1L.getM());
+        %Plot second left joint
+        g_p1L_p2L = SE2([0, -obj.linkLeft(2)], obj.alphaL(2));
         g_torso_p2L = g_torso_p1L * g_p1L_p2L;
         g_torso_p2L.plot('p2L')
         hold on
-        g_p2L_Lf = SE2([obj.linkLeft(3), 0], obj.alphaL(3));
+            %Plot line between first and second left joint
+            obj.plot_limb(g_torso_p1L.getM(), g_p1L_p2L.getM());
+        %Plot left foot joint
+        g_p2L_Lf = SE2([0, -obj.linkLeft(3)], obj.alphaL(3));
         g_t_lf = g_torso_p1L * g_p1L_p2L * g_p2L_Lf;
         plot(g_t_lf, 't_lf')
         hold on
-      
-        g_torso_p1R = obj.torsoSE2 * SE2([obj.linkRight(1), 0], obj.alphaR(1));
+            %Plot line between second left joint and left foot joint
+            obj.plot_limb(g_torso_p2L.getM(), g_p2L_Lf.getM());
+            %Plot foot
+            g_Lf_footB = SE2([-obj.linkLeft(4), 0], 0);
+            obj.plot_limb(g_t_lf.getM(), g_Lf_footB.getM());
+            g_Lf_footF = SE2([obj.linkLeft(5), 0], 0);
+            obj.plot_limb(g_t_lf.getM(), g_Lf_footF.getM());
+        %Plot first right joint
+        g_torso_p1R = obj.torsoSE2 * SE2([0, -obj.linkRight(1)], obj.alphaR(1));
         g_torso_p1R.plot('p1R');
-        g_p1R_p2R = SE2([obj.linkRight(2), 0], obj.alphaR(2));
+            %Plot line between torso and first right joint
+            obj.plot_limb(obj.torsoSE2.getM(), g_torso_p1R.getM());
+        %Plot second right joint
+        g_p1R_p2R = SE2([0, -obj.linkRight(2)], obj.alphaR(2));
         g_torso_p2R = g_torso_p1R * g_p1R_p2R;
         g_torso_p2R.plot('p2R')
         hold on
-        g_p2R_Rf = SE2([obj.linkRight(3), 0], obj.alphaR(3));        
+            %Plot line between first and second right joint
+            obj.plot_limb(g_torso_p1R.getM(), g_p1R_p2R.getM());
+        %Plot right foot joint
+        g_p2R_Rf = SE2([0, -obj.linkRight(3)], obj.alphaR(3));        
         g_t_rf = g_torso_p1R * g_p1R_p2R * g_p2R_Rf;
         g_t_rf.plot('t_rf')
         hold on
+            %Plot line between second right joint and right foot joint
+            obj.plot_limb(g_torso_p2R.getM(), g_p2R_Rf.getM());
+            %Plot foot
+            g_Rf_footB = SE2([-obj.linkRight(4), 0], 0);
+            obj.plot_limb(g_t_rf.getM(), g_Rf_footB.getM());
+            g_Rf_footF = SE2([obj.linkRight(5), 0], 0);
+            obj.plot_limb(g_t_rf.getM(), g_Rf_footF.getM());
     end
   end     % methods
 end
--- test_biped_skeleton.m
+++ test_biped_skeleton.m
@@ -14,26 +14,26 @@ my_link_lens = [2.25, 3.625, 3.625, 0.5, 1.75; 2.25, 3.625, 3.625, 0.5, 1.75];
 my_biped.set_geometry( my_link_lens );
 
 % Joint configuration #1
-my_joint_angles_1 = [0, 0, 0; 0, 0, 0];
+my_joint_angles_1 = [pi/2, -pi/2, -pi/4; 0, 0, 0];
 my_biped.set_alpha( my_joint_angles_1 );
 
 [my_g_t_lf, my_g_t_rf] = my_biped.fk_torso_foot();
     
 my_biped.plotTF();
 
-% % Joint configuration #2
-% my_joint_angles_2 = [];
-% my_biped.set_alpha( my_joint_angles_2 );
-% 
-% [my_g_t_lf, my_g_t_rf] = my_biped.fk_torso_foot();
-%     
-% my_biped.plotTF();
-% 
-% % Joint configuration #3
-% my_joint_angles_3 = [];
-% my_biped.set_alpha( my_joint_angles_3 );
-% 
-% [my_g_t_lf, my_g_t_rf] = my_biped.fk_torso_foot();
-%     
-% my_biped.plotTF();
+% Joint configuration #2
+my_joint_angles_2 = [3*pi/4, 0, 0; 0, -pi/4, 0];
+my_biped.set_alpha( my_joint_angles_2 );
+
+[my_g_t_lf, my_g_t_rf] = my_biped.fk_torso_foot();
+    
+my_biped.plotTF();
+
+% Joint configuration #3
+my_joint_angles_3 = [pi/3, -pi/2, 0; -pi/3, -pi/2, 0];
+my_biped.set_alpha( my_joint_angles_3 );
+
+[my_g_t_lf, my_g_t_rf] = my_biped.fk_torso_foot();
+    
+my_biped.plotTF();
 
